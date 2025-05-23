f25d6382b227ace84da03b9e2c94d573a73eea28
VICTOR-JEON-1998 <jeonseongcheol@naver.com>
2025-05-23 10:35:53 +0900

    chore: restructure project with root Git repo and terraform folder

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,27 +8,27 @@ resource "aws_ecr_repository" "app_repo" {
 
 resource "aws_security_group" "web_sg" {
   name        = "airline-web-sg"
-  description = "Allow SSH, HTTP, HTTPS"
+  description = "Allow SSH, HTTP, HTTPS only from my IP"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["180.64.226.122/32"] # 너의 공인 IP만 허용
+    cidr_blocks = ["180.64.226.122/32"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["180.64.226.122/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["180.64.226.122/32"]
   }
 
   egress {
@@ -40,7 +40,7 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-0c9c942bd7bf113a2" # Ubuntu 22.04 LTS
+  ami                         = "ami-0c9c942bd7bf113a2" # Ubuntu 22.04 LTS in Seoul
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
